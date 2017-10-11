de7d9965f6ff599681672b594cf2588720d195d8
@cristian-Yair <kristian_9999@hotmail.com>
2017-10-11 14:31:02 -0500

    Initial commit

---
+++ app/src/main/java/krist/example/com/parqueaderoa1200/MainActivity.java
@@ -0,0 +1,13 @@
+package krist.example.com.parqueaderoa1200;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
